802c9eb91ebce7a3a22bfc499f2a801eeec07ebe
cjmcentee <cjmcentee@gmail.com>
2014-08-25 15:54:05 -0700

    another try at fixing gitignore

--- src/main/java/src/ddpsc/database/tile/LTSystem.java
+++
@@ -1,8 +0,0 @@
-package src.ddpsc.database.tile;
-
-public class LTSystem
-{
-	public final static String fileroot = "/data/pgftp";
-	
-	//public final static String fileroot = "C:/Development/DDPSC/PostgreSQL/9.3/data/pgftp"; // TODO Make sure Steven doesn't get this, Steven if you're reading this, I am SO SORRY!
-}
